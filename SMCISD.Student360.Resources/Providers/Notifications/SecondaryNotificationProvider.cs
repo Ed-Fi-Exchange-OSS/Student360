27dd05dcbb0694f0c849d94a14621785d31921e0
Stephen A. Fuqua <stephen@safnet.com>
2021-04-22 13:31:31 -0500

    License headers

--- SMCISD.Student360.Resources/Providers/Notifications/SecondaryNotificationProvider.cs
+++ SMCISD.Student360.Resources/Providers/Notifications/SecondaryNotificationProvider.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using Microsoft.AspNetCore.Hosting;
 using SMCISD.Student360.Resources.Providers.Messaging;
 using SMCISD.Student360.Resources.Services.StudentAbsencesForEmail;
 using System.IO;
